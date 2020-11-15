fc254e045bbc007093b997e45b114a82691cfa31
kakashi-go <nk29time@gmail.com>
2020-11-15 19:26:46 +0900

    dev3

--- src/router/index.js
+++ src/router/index.js
@@ -19,14 +19,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/insurance.vue')
   },
-  {
-  path: '/consultation',
-  name: 'consultation',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '../views/consultation.vue')
- },
  {
   path: '/consultation',
   name: 'consultation',
